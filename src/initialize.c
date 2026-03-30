694e5e36194212d6d7f22f62a8fa22a635da104e
Mykiee007 <velasquez.michaelchristopher@gmail.com>
2026-03-30 13:44:32 +0800

    load assets function

--- src/initialize.c
+++ src/initialize.c
@@ -28,3 +28,25 @@ void	init_so_long(t_map *map_info, t_unit *unit_info)
 	unit_info->x_player = 0;
 	unit_info->y_player = 0;
 }
+
+void	load_assets(t_game *game)
+{
+	game->assets.player.img = mlx_xpm_file_to_image(
+		game->mlx, "assets/player.xpm",
+		&game->assets.player.width, &game->assets.player.height);
+	game->assets.wall.img = mlx_xpm_file_to_image(
+		game->mlx, "assets/wall.xpm",
+		&game->assets.wall.width, &game->assets.wall.height);
+	game->assets.space.img = mlx_xpm_file_to_image(
+		game->mlx, "assets/floor.xpm",
+		&game->assets.space.width, &game->assets.space.height);
+	game->assets.collectible.img = mlx_xpm_file_to_image(
+		game->mlx, "assets/chest.xpm",
+		&game->assets.collectible.width, &game->assets.collectible.height);
+	game->assets.exit.img = mlx_xpm_file_to_image(
+		game->mlx, "assets/exit.xpm",
+		&game->assets.exit.width, &game->assets.exit.height);
+	game->assets.enemy.img = mlx_xpm_file_to_image(
+		game->mlx, "assets/enemy.xpm",
+		&game->assets.enemy.width, &game->assets.enemy.height);
+}
