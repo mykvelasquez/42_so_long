8877e3be361aaafb1d1a384694398c581ee5dd71
Mykiee007 <velasquez.michaelchristopher@gmail.com>
2026-04-04 04:59:14 +0800

    fix Collectible and Exit check for finding path

--- src/map_validation_3.c
+++ src/map_validation_3.c
@@ -73,6 +73,26 @@ static void flood_fill(t_map *map_info, t_unit unit_info, int y, int x)
 	flood_fill(map_info, unit_info, y, x - 1);
 }
 
+static int	map_has_char(char **map_dup, char c)
+{
+	int		i;
+	int 	j;
+
+	i = 0;
+	while (map_dup[i])
+	{
+		j = 0;
+		while (map_dup[i][j])
+		{
+			if (map_dup[i][j] == c)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 int map_check_find_path(t_map *map_info, t_unit *unit_info)
 {
 	char **m_dup;
@@ -83,11 +103,11 @@ int map_check_find_path(t_map *map_info, t_unit *unit_info)
 		return (-1);
 	if (find_exit(map_info, unit_info) == -1)
 		return (-1);
-	flood_fill(map_info, *unit_info, unit_info->y_player, unit_info->x_player);
 	m_dup = map_info->map_str_dup;
-	if (map_check_req_char(m_dup, unit_info, unit_info->exit) == 0)
+	flood_fill(map_info, *unit_info, unit_info->y_player, unit_info->x_player);
+	if (map_has_char(m_dup, unit_info->exit))
 		return (ft_putendl_fd("Error: Exit inaccessible.", 2), -1);
-	if (map_check_req_char(m_dup, unit_info, unit_info->collectible) == 0)
+	if (map_has_char(m_dup, unit_info->collectible))
 		return (ft_putendl_fd("Error: Collectible inaccessible.", 2), -1);
 	return (0);
 }
