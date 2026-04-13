c2a92d5efde059516fcc3ec2ac775a175fe67c48
Mykiee007 <velasquez.michaelchristopher@gmail.com>
2026-04-13 11:19:45 +0800

    fix return after game is initialied and errors occur

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 	if (start_so_long(argc, argv, &map_info, &unit_info) == -1)
 		return (map_free(&map_info), 1);
 	if (start_game_so_long(&game, &map_info, &unit_info) == -1)
-		return (game_cleanup(&game), map_free(&map_info), 1);
+		return (game_exit(&game), 1);
 	game_render_map(&game);
 	mlx_hook(game.win, 2, 1L << 0, game_key_handler, &game);
 	mlx_hook(game.win, 17, 0, game_key_close_x, &game);
